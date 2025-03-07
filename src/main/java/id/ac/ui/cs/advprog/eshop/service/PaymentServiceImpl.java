0ac7e7e635604fac3d8809a5c6d030470c3adbec
agtyra <keziagtyra@gmail.com>
2025-03-07 20:16:26 +0700

    [RED] Add PaymentServiceImpl skeleton

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
@@ -0,0 +1,34 @@
+package id.ac.ui.cs.advprog.eshop.service;
+
+import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
+import id.ac.ui.cs.advprog.eshop.model.Order;
+import id.ac.ui.cs.advprog.eshop.model.Payment;
+import id.ac.ui.cs.advprog.eshop.repository.PaymentRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+@Service
+public class PaymentServiceImpl implements PaymentService {
+    private final PaymentRepository paymentRepository;
+
+    public PaymentServiceImpl(PaymentRepository paymentRepository) {
+        this.paymentRepository = paymentRepository;
+    }
+
+    public Payment addPayment(Order order, String method, Map<String, String> paymentData) {
+        return null;
+    }
+
+    public void setStatus(Payment payment, String status) {}
+
+    public Payment getPayment(String paymentId) {
+        return null;
+    }
+
+    public List<Payment> getAllPayments() {
+        return null;
+    }
+}
