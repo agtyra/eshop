5974c40b50985b0bf3c09d4d419e795d89b34950
agtyra <keziagtyra@gmail.com>
2025-03-07 18:14:56 +0700

    [REFACTOR] Apply PaymentMethod and PaymentStatus enum check to Payment model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -1,5 +1,7 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
+import id.ac.ui.cs.advprog.eshop.enums.PaymentMethod;
+import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
 import lombok.Getter;
 
 import java.util.Map;
@@ -12,40 +14,45 @@ public class Payment {
     private String status;
 
     public Payment(String id, String method, Map<String, String> paymentData) {
+
         this.id = id;
+        if (!PaymentMethod.contains(method)) {
+            throw new IllegalArgumentException("Invalid payment method: " + method);
+        }
         this.method = method;
         this.paymentData = paymentData;
         this.status = validatePayment(method, paymentData);
     }
 
+
     private String validatePayment(String method, Map<String, String> paymentData) {
-        if ("Voucher".equalsIgnoreCase(method)) {
+        if (method.equalsIgnoreCase(PaymentMethod.VOUCHER.getValue())) {
             return validateVoucher(paymentData);
-        } else if ("BankTransfer".equalsIgnoreCase(method)) {
+        } else if (method.equalsIgnoreCase(PaymentMethod.BANK_TRANSFER.getValue())) {
             return validateBankTransfer(paymentData);
         }
-        return "REJECTED";
+        return PaymentStatus.REJECTED.getValue();
     }
 
     private String validateVoucher(Map<String, String> data) {
         String code = data.get("voucherCode");
         if (code != null && code.length() == 16 && code.startsWith("ESHOP") && code.replaceAll("\\D", "").length() == 8) {
-            return "SUCCESS";
+            return PaymentStatus.SUCCESS.getValue();
         }
-        return "REJECTED";
+        return PaymentStatus.REJECTED.getValue();
     }
 
     private String validateBankTransfer(Map<String, String> data) {
         String bankName = data.get("bankName");
         String referenceCode = data.get("referenceCode");
         if (bankName == null || bankName.isEmpty() || referenceCode == null || referenceCode.isEmpty()) {
-            return "REJECTED";
+            return PaymentStatus.REJECTED.getValue();
         }
-        return "SUCCESS";
+        return PaymentStatus.SUCCESS.getValue();
     }
 
     public void setStatus(String status) {
-        if (status.equals("SUCCESS") || status.equals("REJECTED")) {
+        if (PaymentStatus.contains(status)) {
             this.status = status;
         } else {
             throw new IllegalArgumentException("Invalid payment status.");
